e307f43d57ad0c918c3010317469b5739c1037ce
Ashley Weber <aweber333@yahoo.com>
2018-08-12 18:43:06 -0400

    final

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
-
+app.use(express.static(__dirname + "/app/public"));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
